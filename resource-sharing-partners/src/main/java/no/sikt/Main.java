85924ca9873641a667697348bf3f355254216d71
anetteOlli <anette.olli.siiri@sikt.no>
2022-04-20 15:27:24 +0200

    build checks

--- resource-sharing-partners/src/main/java/no/sikt/Main.java
+++
@@ -1,8 +0,0 @@
-package no.sikt;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ resource-sharing-partners/src/test/java/no/sikt/rsp/ResourceSharingPartnerTest.java
@@ -0,0 +1,12 @@
+package no.sikt.rsp;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ResourceSharingPartnerTest {
+
+    @Test
+    public void skeletonTest() {
+        ResourceSharingPartnerHandler resourceSharingPartnerHandler = new ResourceSharingPartnerHandler();
+    }
+}
