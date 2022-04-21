7df3bbba8a0daecc49b44ce418f6e68c3544f5b9
anetteOlli <anette.olli.siiri@sikt.no>
2022-04-21 13:19:53 +0200

    adding cloudformation yml file

--- resource-sharing-partners/src/main/java/no/sikt/rsp/ResourceSharingPartnerHandler.java
+++ resource-sharing-partners/src/main/java/no/sikt/rsp/ResourceSharingPartnerHandler.java
@@ -1,5 +1,14 @@
 package no.sikt.rsp;
 
-public class ResourceSharingPartnerHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.Map;
+
+public class ResourceSharingPartnerHandler implements RequestHandler<Map<String, Object>, String> {
         //skeleton
+
+    @Override
+    public String handleRequest(Map<String, Object> s3Event, Context context) {
+        return "";
+    }
 }
