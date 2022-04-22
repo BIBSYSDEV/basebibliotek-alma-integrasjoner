feab06b718fad092377476da56a097c62c9d5ef5
anetteOlli <anette.olli.siiri@sikt.no>
2022-04-22 11:02:46 +0200

    testing lamdalogger

--- resource-sharing-partners/src/main/java/no/sikt/rsp/ResourceSharingPartnerHandler.java
+++ resource-sharing-partners/src/main/java/no/sikt/rsp/ResourceSharingPartnerHandler.java
@@ -1,18 +1,17 @@
 package no.sikt.rsp;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ResourceSharingPartnerHandler implements RequestHandler<Map<String, Object>, String> {
-    private static final Logger logger = LoggerFactory.getLogger(ResourceSharingPartnerHandler.class);
         //skeleton
 
     @Override
     public String handleRequest(Map<String, Object> s3Event, Context context) {
-        logger.info("hello world");
+        LambdaLogger lambdaLogger = context.getLogger();
+        lambdaLogger.log("hello world");
         return "";
     }
 }
