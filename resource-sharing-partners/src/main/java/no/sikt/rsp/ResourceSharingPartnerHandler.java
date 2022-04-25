6e39152f646fdb94f43afe403641954e5ca2acb0
anetteOlli <anette.olli.siiri@sikt.no>
2022-04-25 16:10:19 +0200

    testing

--- resource-sharing-partners/src/main/java/no/sikt/rsp/ResourceSharingPartnerHandler.java
+++ resource-sharing-partners/src/main/java/no/sikt/rsp/ResourceSharingPartnerHandler.java
@@ -5,13 +5,14 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 public class ResourceSharingPartnerHandler implements RequestHandler<Map<String, Object>, String> {
     private static final Logger log = LoggerFactory.getLogger(ResourceSharingPartnerHandler.class);
         //skeleton
 
     @Override
     public String handleRequest(Map<String, Object> s3Event, Context context) {
+        var lambdalogger = context.getLogger();
+        lambdalogger.log("lambdalogger");
         log.info("Starting lambda");
         log.warn("testing warning");
         log.debug("testing debug");
