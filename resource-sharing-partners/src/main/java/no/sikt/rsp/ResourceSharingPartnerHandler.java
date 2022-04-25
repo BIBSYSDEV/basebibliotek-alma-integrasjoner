baa71af915cc9effc46036062d4390c5708a8db9
anetteOlli <anette.olli.siiri@sikt.no>
2022-04-25 11:26:45 +0200

    testing different version of slf4j

--- resource-sharing-partners/src/main/java/no/sikt/rsp/ResourceSharingPartnerHandler.java
+++ resource-sharing-partners/src/main/java/no/sikt/rsp/ResourceSharingPartnerHandler.java
@@ -1,17 +1,18 @@
 package no.sikt.rsp;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ResourceSharingPartnerHandler implements RequestHandler<Map<String, Object>, String> {
+    private static final Logger log = LoggerFactory.getLogger(ResourceSharingPartnerHandler.class);
         //skeleton
 
     @Override
     public String handleRequest(Map<String, Object> s3Event, Context context) {
-        LambdaLogger lambdaLogger = context.getLogger();
-        lambdaLogger.log("hello world");
+        log.info("hello world");
         return "";
     }
 }
