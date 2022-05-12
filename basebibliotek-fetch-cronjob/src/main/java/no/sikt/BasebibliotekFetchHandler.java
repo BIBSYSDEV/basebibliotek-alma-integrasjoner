615afa908cdab9bb78c10f4ae06126f8d5a2938d
anetteOlli <anette.olli.siiri@sikt.no>
2022-05-12 15:25:15 +0200

    SMILE-1547-basebibliotek-cron, functionality implemented

--- basebibliotek-fetch-cronjob/src/main/java/no/sikt/BasebibliotekFetchHandler.java
+++ basebibliotek-fetch-cronjob/src/main/java/no/sikt/BasebibliotekFetchHandler.java
@@ -1,26 +1,35 @@
 package no.sikt;
 
+import static nva.commons.core.attempt.Try.attempt;
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
-import com.amazonaws.services.lambda.runtime.Context;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import no.unit.nva.language.tooling.JacocoGenerated;
 import no.unit.nva.s3.S3Driver;
+import nva.commons.core.Environment;
 import nva.commons.core.paths.UriWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import nva.commons.core.Environment;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class BasebibliotekFetchHandler implements RequestHandler<ScheduledEvent, String> {
 
     private static final Logger logger = LoggerFactory.getLogger(BasebibliotekFetchHandler.class);
+    public static final String FILENAME_REGEX = ".*?\">(bb-.*?.xml)</a>.*";
 
     private final S3Client s3Client;
     private final HttpClient httpClient;
@@ -29,14 +38,18 @@ public class BasebibliotekFetchHandler implements RequestHandler<ScheduledEvent,
     public static final String BASEBIBLILOTEK_USERNAME_ENVIRONMENT_NAME = "BASEBIBLIOTEK_USERNAME";
     public static final String BASEBIBLIOTEK_PASSWORD_ENVIRONMENT_NAME = "BASEBIBLIOTEK_PASSWORD";
     public static final String S3_BUCKET_ENVIRONMENT_NAME = "BASEBIBLIOTEK_XML_BUCKET";
-    public final String BASEBIBLIOTEK_URI;
-    public final String BASEBIBLIOTEK_USERNAME;
-    public final String BASEBIBLIOTEK_PASSWORD;
-    public final String S3_BASEBIBLIOTEK_XML_BUCKET;
-    private final String IO_EXCEPTION_MESSAGE = "Could not contact basebibliotek";
+    public final String basebibliotekUri;
+    public final String basebibliotekUsername;
+    public final String basebibliotekPassword;
+    public final String s3BasebibliotekXmlBucket;
+    private static final String IO_EXCEPTION_MESSAGE = "Could not contact basebibliotek";
 
     private final String BASEBIBLIOTEK_RESPONSE_STATUS_ERROR = "could not connect to basebibliotek, Connection "
                                                                + "responded with status: ";
+
+    private static final String IMPORT_ALL_LIBRARIES = "bb-full.xml";
+
+    private final String basebibliotekAuthorization;
 
     @JacocoGenerated
     public BasebibliotekFetchHandler() {
@@ -47,23 +60,31 @@ public class BasebibliotekFetchHandler implements RequestHandler<ScheduledEvent,
     public BasebibliotekFetchHandler(S3Client s3Client, HttpClient httpClient, Environment environment) {
         this.s3Client = s3Client;
         this.httpClient = httpClient;
-        this.BASEBIBLIOTEK_URI = environment.readEnv(BASEBIBLIOTEK_URI_ENVIRONMENT_NAME);
-        this.BASEBIBLIOTEK_USERNAME = environment.readEnv(BASEBIBLILOTEK_USERNAME_ENVIRONMENT_NAME);
-        this.BASEBIBLIOTEK_PASSWORD = environment.readEnv(BASEBIBLIOTEK_PASSWORD_ENVIRONMENT_NAME);
-        this.S3_BASEBIBLIOTEK_XML_BUCKET = environment.readEnv(S3_BUCKET_ENVIRONMENT_NAME);
+        this.basebibliotekUri = environment.readEnv(BASEBIBLIOTEK_URI_ENVIRONMENT_NAME);
+        this.basebibliotekUsername = environment.readEnv(BASEBIBLILOTEK_USERNAME_ENVIRONMENT_NAME);
+        this.basebibliotekPassword = environment.readEnv(BASEBIBLIOTEK_PASSWORD_ENVIRONMENT_NAME);
+        this.s3BasebibliotekXmlBucket = environment.readEnv(S3_BUCKET_ENVIRONMENT_NAME);
+        this.basebibliotekAuthorization = createAuthorization();
+    }
+
+    private String createAuthorization() {
+        String loginPassword = basebibliotekUsername + ":" + basebibliotekPassword;
+        return String.format("Basic %s", Base64.getEncoder().encodeToString(loginPassword.getBytes()));
     }
 
     @Override
     public String handleRequest(ScheduledEvent scheduledEvent, Context context) {
         try {
-            var request = createRequest();
+            var request = createRequest(UriWrapper.fromUri(basebibliotekUri).getUri());
             HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
             if (response.statusCode() != HttpURLConnection.HTTP_OK) {
-                logger.info(BASEBIBLIOTEK_RESPONSE_STATUS_ERROR  + response.statusCode());
+                logger.info(BASEBIBLIOTEK_RESPONSE_STATUS_ERROR + response.statusCode());
                 throw new RuntimeException();
+            } else {
+                var filenames = snipIncrementalBasebibliotekUrls(response.body());
+                var basebibliotekXmls = fetchBasebibliotekXmls(filenames);
 
-            }else {
-                var url = snipIncrementalBasebibliotekUrls(response.body());
+                putObjectstoS3(basebibliotekXmls);
             }
             return response.body();
         } catch (IOException | InterruptedException e) {
@@ -72,15 +93,91 @@ public class BasebibliotekFetchHandler implements RequestHandler<ScheduledEvent,
         }
     }
 
-    private List<String> snipIncrementalBasebibliotekUrls(String body) {
-
-        return null;
+    private void putObjectstoS3(List<BasebibliotekXmlAndFileName> basebibliotekXmlAndFileNames) {
+        basebibliotekXmlAndFileNames.forEach(this::putObjectToS3);
     }
 
-    private HttpRequest createRequest() {
+    private void putObjectToS3(BasebibliotekXmlAndFileName basebibliotekXmlAndFileName) {
+        attempt(() -> s3Client.putObject(createPutObjectRequest(basebibliotekXmlAndFileName.getFileName()),
+                                         RequestBody.fromString(basebibliotekXmlAndFileName.getBasebibliotekXmlBody())))
+            .orElseThrow(fail -> logS3ExpectionAndThrowRuntimeError(fail.getException()))
+        ;
+    }
+
+    private RuntimeException logS3ExpectionAndThrowRuntimeError(Exception exception) {
+        logger.info("Could not upload file to s3");
+        return exception instanceof RuntimeException
+                   ? (RuntimeException) exception
+                   : new RuntimeException(exception);
+    }
+
+    private PutObjectRequest createPutObjectRequest(String filename) {
+        return PutObjectRequest.builder()
+                   .bucket(s3BasebibliotekXmlBucket)
+                   .key(filename)
+                   .build();
+    }
+
+    private List<BasebibliotekXmlAndFileName> fetchBasebibliotekXmls(List<String> filenames) {
+        return filenames.stream()
+                   .map(this::fetchBasebibliotekXml)
+                   .flatMap(Optional::stream)
+                   .collect(Collectors.toList());
+    }
+
+    private Optional<BasebibliotekXmlAndFileName> fetchBasebibliotekXml(String filename) {
+        try {
+            var request = createRequest(UriWrapper.fromUri(basebibliotekUri + "/" + filename).getUri());
+            HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+            if (response.statusCode() != HttpURLConnection.HTTP_OK) {
+                logger.info(BASEBIBLIOTEK_RESPONSE_STATUS_ERROR + response.statusCode());
+                return Optional.empty();
+            } else {
+                return Optional.of(new BasebibliotekXmlAndFileName(filename, response.body()));
+            }
+        } catch (IOException | InterruptedException e) {
+            logger.info(IO_EXCEPTION_MESSAGE, e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<String> snipIncrementalBasebibliotekUrls(String body) {
+        Pattern p = Pattern.compile(FILENAME_REGEX);
+        return p.matcher(body)
+                   .results()
+                   .map(matcher -> matcher.group(1))
+                   .filter(filename -> !isFullBasebibliotekFileName(filename))
+                   .collect(Collectors.toList());
+    }
+
+    private boolean isFullBasebibliotekFileName(String filename) {
+        return IMPORT_ALL_LIBRARIES.equalsIgnoreCase(filename);
+    }
+
+    private HttpRequest createRequest(URI uri) {
         return HttpRequest.newBuilder()
-                   .uri(UriWrapper.fromUri(BASEBIBLIOTEK_URI).getUri())
+                   .uri(uri)
+                   .setHeader("Authorization", basebibliotekAuthorization)
                    .GET()
                    .build();
+    }
+
+    /* default */class BasebibliotekXmlAndFileName {
+
+        private final String fileName;
+        private final String basebibliotekXmlBody;
+
+        public BasebibliotekXmlAndFileName(String fileName, String basebibliotekXmlBody) {
+            this.fileName = fileName;
+            this.basebibliotekXmlBody = basebibliotekXmlBody;
+        }
+
+        public String getFileName() {
+            return fileName;
+        }
+
+        public String getBasebibliotekXmlBody() {
+            return basebibliotekXmlBody;
+        }
     }
 }
--- basebibliotek-fetch-cronjob/src/test/java/no/sikt/BaseBibliotekFetchHandlerTest.java
+++ basebibliotek-fetch-cronjob/src/test/java/no/sikt/BaseBibliotekFetchHandlerTest.java
@@ -5,26 +5,26 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.when;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.matching.UrlPattern;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.http.HttpClient;
 import java.nio.file.Path;
-import no.unit.nva.s3.S3Driver;
-import no.unit.nva.stubs.FakeS3Client;
+import java.util.Arrays;
 import no.unit.nva.stubs.WiremockHttpClient;
 import nva.commons.core.Environment;
 import nva.commons.core.ioutils.IoUtils;
@@ -33,53 +33,46 @@ import nva.commons.logutils.TestAppender;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mockito;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class BaseBibliotekFetchHandlerTest {
 
-    // Sjekker at den ignorerer full basebibliotek-xml url
-    // Sjekke at den kontakter de andre url'ene og ender tilslutt opp med basebibliotek-xml
-    //Håndtering av feil i connection
-    //Skrive til s3
-    //Håndtere s3 problemer.
-
-    public static final String FILE_URI_TEMPLATE = "http://localhost:%d/file/%s";
-    public static final String BIBLIOTEK_EKSPORT_BIBLEV_BB_2022_04_27_XML = "/bibliotek/eksport/biblev/bb-2022-04-27"
-                                                                            + ".xml";
-
-    public static final String BIBLIOTEK_EKSPORT_BIBLEV_BB_2022_05_04_XML = "/bibliotek/eksport/biblev/bb-2022-05-04"
-                                                                           + ".xml";
-    public static final String BASEBIBLIOTEK_XML_FULL = "/bibliotek/eksport/biblev/bb-full.xml";
+    public static final String BASEBIBLIOTEK_REDACTED_INCREMENTAL_1_XML = "basebibliotek_redacted_incremental_1.xml";
+    public static final String BASEBIBLIOTEK_REDACTED_INCREMENTAL_2_XML = "basebibliotek_redacted_incremental_2.xml";
+    private static final String S3_BUCKET_NAME = "s3BucketName";
+    public static final String BIBLIOTEK_EKSPORT_BIBLEV_PATH = "/bibliotek/eksport/biblev";
+    private static final String BASEBIBLIOTEK_BB_2022_04_27_XML = "bb-2022-04-27.xml";
+    private static final String BASEBIBLIOTEK_BB_2022_05_04_XML = "bb-2022-05-04.xml";
+    private static final String BASEBIBLIOTEK_BB_FULL_XML = "bb-full.xml";
     private WireMockServer httpServer;
-    private HttpClient httpClient;
 
     private BasebibliotekFetchHandler baseBibliotekFetchHandler;
     public static final Context CONTEXT = mock(Context.class);
 
-    private FakeS3Client s3Client;
-    private S3Driver s3Driver;
-
-    private Environment environment;
+    private S3Client s3Client;
 
     private TestAppender appender;
 
     @BeforeEach
     public void init() {
         appender = LogUtils.getTestingAppenderForRootLogger();
-        s3Client = new FakeS3Client();
-        s3Driver = new S3Driver(s3Client, "ignoredValue");
+        s3Client = mock(S3Client.class);
         startWiremockServer();
-        httpClient = WiremockHttpClient.create();
-        environment = mock(Environment.class);
+        HttpClient httpClient = WiremockHttpClient.create();
+        Environment environment = mock(Environment.class);
         when(environment.readEnv(BasebibliotekFetchHandler.BASEBIBLIOTEK_URI_ENVIRONMENT_NAME)).thenReturn(
-            "http://localhost"
-            + ":"
+            "http://localhost:"
             + httpServer.port()
             + "/bibliotek/eksport/biblev");
         when(environment.readEnv(BasebibliotekFetchHandler.BASEBIBLILOTEK_USERNAME_ENVIRONMENT_NAME)).thenReturn(
             "ignored");
         when(environment.readEnv(BasebibliotekFetchHandler.BASEBIBLIOTEK_PASSWORD_ENVIRONMENT_NAME)).thenReturn(
             "ignored");
-        when(environment.readEnv(BasebibliotekFetchHandler.S3_BUCKET_ENVIRONMENT_NAME)).thenReturn("ignored");
+        when(environment.readEnv(BasebibliotekFetchHandler.S3_BUCKET_ENVIRONMENT_NAME)).thenReturn(S3_BUCKET_NAME);
         baseBibliotekFetchHandler = new BasebibliotekFetchHandler(s3Client, httpClient, environment);
     }
 
@@ -101,33 +94,80 @@ public class BaseBibliotekFetchHandlerTest {
 
     @Test
     public void shouldConnectToBasebibliotek() {
-        var scheduledEvent = new ScheduledEvent();
-        var basebibliotekUrlsAsHTML = IoUtils.stringFromResources(
+
+        var basebibliotekUrlsAsHtml = IoUtils.stringFromResources(
             Path.of("basebibliotek-url.html"));
-        mockedGetRequestThatReturnsFile(basebibliotekUrlsAsHTML);
-        mockedBasebibliotekXmlResponse();
-        var response = baseBibliotekFetchHandler.handleRequest(scheduledEvent, CONTEXT);
-        verify( getRequestedFor(urlEqualTo(BIBLIOTEK_EKSPORT_BIBLEV_BB_2022_04_27_XML)));
-        verify( getRequestedFor(urlEqualTo(BIBLIOTEK_EKSPORT_BIBLEV_BB_2022_05_04_XML)));
-        verify(0, getRequestedFor(urlEqualTo(BASEBIBLIOTEK_XML_FULL)));
-        assertThat(response, is(equalTo(basebibliotekUrlsAsHTML)));
+        mockedGetRequestThatReturnsSpecifiedResponse(basebibliotekUrlsAsHtml);
+
+        var basebibliotekXML1 = IoUtils.stringFromResources(
+            Path.of(BASEBIBLIOTEK_REDACTED_INCREMENTAL_1_XML));
+        var basebibliotekXML2 = IoUtils.stringFromResources(
+            Path.of(BASEBIBLIOTEK_REDACTED_INCREMENTAL_2_XML));
+        mockedWiremockStubFor(BIBLIOTEK_EKSPORT_BIBLEV_PATH + "/" + BASEBIBLIOTEK_BB_2022_04_27_XML, basebibliotekXML1);
+        mockedWiremockStubFor(BIBLIOTEK_EKSPORT_BIBLEV_PATH + "/" + BASEBIBLIOTEK_BB_2022_05_04_XML, basebibliotekXML2);
+
+        var scheduledEvent = new ScheduledEvent();
+        baseBibliotekFetchHandler.handleRequest(scheduledEvent, CONTEXT);
+
+        //Verify that basebibliotek has been contacted.
+        WireMock.verify(getRequestedFor(urlEqualTo(BIBLIOTEK_EKSPORT_BIBLEV_PATH)));
+        WireMock.verify(
+            getRequestedFor(urlEqualTo(BIBLIOTEK_EKSPORT_BIBLEV_PATH + "/" + BASEBIBLIOTEK_BB_2022_04_27_XML)));
+        WireMock.verify(
+            getRequestedFor(urlEqualTo(BIBLIOTEK_EKSPORT_BIBLEV_PATH + "/" + BASEBIBLIOTEK_BB_2022_05_04_XML)));
+        WireMock.verify(0,
+                        getRequestedFor(urlEqualTo(BIBLIOTEK_EKSPORT_BIBLEV_PATH + "/" + BASEBIBLIOTEK_BB_FULL_XML)));
+
+        //verify that the s3client has been called to putobjects:
+        Mockito
+            .verify(this.s3Client)
+            .putObject(eq(generatePutObjectRequest(BASEBIBLIOTEK_BB_2022_04_27_XML)),
+                       argThat(new RequestBodyMatches(RequestBody.fromString(basebibliotekXML1))));
+        Mockito
+            .verify(this.s3Client).putObject(eq(generatePutObjectRequest(BASEBIBLIOTEK_BB_2022_05_04_XML)),
+                                             argThat(
+                                                 new RequestBodyMatches(RequestBody.fromString(basebibliotekXML2))));
+        Mockito
+            .verify(this.s3Client, never()).putObject(eq(generatePutObjectRequest(BASEBIBLIOTEK_BB_FULL_XML)),
+                                                      any(RequestBody.class));
     }
 
-    /*
     @Test
-    public void shouldContactBasebibliotek() {
+    public void shouldHandleS3Exceptions() {
+        var basebibliotekUrlsAsHtml = IoUtils.stringFromResources(
+            Path.of("basebibliotek-url.html"));
+        mockedGetRequestThatReturnsSpecifiedResponse(basebibliotekUrlsAsHtml);
 
+        var basebibliotekXML1 = IoUtils.stringFromResources(
+            Path.of(BASEBIBLIOTEK_REDACTED_INCREMENTAL_1_XML));
+        var basebibliotekXML2 = IoUtils.stringFromResources(
+            Path.of(BASEBIBLIOTEK_REDACTED_INCREMENTAL_2_XML));
+        mockedWiremockStubFor(BIBLIOTEK_EKSPORT_BIBLEV_PATH + "/" + BASEBIBLIOTEK_BB_2022_04_27_XML, basebibliotekXML1);
+        mockedWiremockStubFor(BIBLIOTEK_EKSPORT_BIBLEV_PATH + "/" + BASEBIBLIOTEK_BB_2022_05_04_XML, basebibliotekXML2);
+
+        var scheduledEvent = new ScheduledEvent();
+        baseBibliotekFetchHandler.handleRequest(scheduledEvent, CONTEXT);
+        when(this.s3Client.putObject(any(PutObjectRequest.class), any(RequestBody.class)))
+            .thenThrow(new RuntimeException());
+        var expectedMessage = "Could not upload file to s3";
+        assertThrows(RuntimeException.class, () -> baseBibliotekFetchHandler.handleRequest(scheduledEvent, CONTEXT));
+        assertThat(appender.getMessages(), containsString(expectedMessage));
     }
 
-     */
+    private PutObjectRequest generatePutObjectRequest(String key) {
+        return PutObjectRequest.builder()
+                   .bucket(S3_BUCKET_NAME)
+                   .key(key)
+                   .build();
+    }
 
     private void startWiremockServer() {
         httpServer = new WireMockServer(options().dynamicHttpsPort());
         httpServer.start();
     }
 
-    private void mockedGetRequestThatReturnsFile(String response) {
-        stubFor(get(urlEqualTo("/bibliotek/eksport/biblev"))
+    private void mockedGetRequestThatReturnsSpecifiedResponse(String response) {
+        stubFor(get(urlEqualTo(BIBLIOTEK_EKSPORT_BIBLEV_PATH))
                     .willReturn(aResponse()
                                     .withHeader(CONTENT_TYPE, "text/html")
                                     .withStatus(HttpURLConnection.HTTP_OK)
@@ -135,21 +175,39 @@ public class BaseBibliotekFetchHandlerTest {
     }
 
     private void mockedGetRequestWithSpecifiedStatusCode(int statusCode) {
-        stubFor(get(urlEqualTo("/bibliotek/eksport/biblev"))
+        stubFor(get(urlEqualTo(BIBLIOTEK_EKSPORT_BIBLEV_PATH))
                     .willReturn(aResponse()
                                     .withHeader(CONTENT_TYPE, "text/html")
                                     .withStatus(statusCode)
                                     .withBody("fdgfg")));
     }
 
-    private void mockedBasebibliotekXmlResponse() {
-        var basebibliotekXML = IoUtils.stringFromResources(
-            Path.of("basebibliotek_redacted_incremental.xml"));
-        stubFor(get(urlEqualTo(BIBLIOTEK_EKSPORT_BIBLEV_BB_2022_04_27_XML))
+    private void mockedWiremockStubFor(String urlEqualTo, String body) {
+        stubFor(get(urlEqualTo(urlEqualTo))
                     .willReturn(aResponse()
                                     .withHeader(CONTENT_TYPE, "application/xml")
                                     .withStatus(HttpURLConnection.HTTP_OK)
-                                    .withBody(basebibliotekXML)));
+                                    .withBody(body)));
+    }
 
+    class RequestBodyMatches implements ArgumentMatcher<RequestBody> {
+
+        private final RequestBody left;
+
+        public RequestBodyMatches(RequestBody left) {
+            this.left = left;
+        }
+
+        @Override
+        public boolean matches(RequestBody right) {
+            try (var rightStream = right.contentStreamProvider().newStream();
+                var leftStream = left.contentStreamProvider().newStream()) {
+                var rightContent = Arrays.toString(rightStream.readAllBytes());
+                var leftContent = Arrays.toString(leftStream.readAllBytes());
+                return rightContent.equals(leftContent);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
