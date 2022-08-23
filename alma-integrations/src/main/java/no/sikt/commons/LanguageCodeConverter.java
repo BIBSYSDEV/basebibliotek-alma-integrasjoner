75e01df9a894765d36d749c1d78ea41cbc00c84d
anetteOlli <anette.olli.siiri@sikt.no>
2022-08-23 12:56:33 +0200

    SMILE-1674-max-size-bibnr-file-from-cron-job, code-review

--- alma-integrations/src/main/java/no/sikt/commons/LanguageCodeConverter.java
+++ alma-integrations/src/main/java/no/sikt/commons/LanguageCodeConverter.java
@@ -1,34 +1,29 @@
 package no.sikt.commons;
 
-import static org.apache.commons.lang3.StringUtils.join;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.MissingResourceException;
 
 public class LanguageCodeConverter {
 
-    public static final String LOCALES_WITHOUT_ISO_3_COUNTRY_CODES = "Locales without ISO3Country-codes: ";
-    public static final String COMMA_SEPARATOR = ", ";
     public static final Map<String, String> isoAlpha2ToIsoAlpha3CodeMap = new HashMap<>();
 
     static {
         Locale[] availableLocales = Locale.getAvailableLocales();
-        List<String> localesWithoutISO3Country = new ArrayList<>();
         for (Locale locale : availableLocales) {
             try {
                 isoAlpha2ToIsoAlpha3CodeMap.put(locale.getCountry(), locale.getISO3Country());
             } catch (MissingResourceException e) {
-                localesWithoutISO3Country.add(locale.toString());
                 // ignore, is useless anyway
+                // Locales without ISO3Country-codes: ji_001, vo_001, sr_CS, en_150, sr_XK_#Cyrl, sr_XK_#Latn,
+                // prg_001, es_EA, sq_XK, eo_001, en_DG, es_419, en_001, es_IC, ar_001
+                // None of these are used by basebibliotek.
             }
         }
-        System.out.println(LOCALES_WITHOUT_ISO_3_COUNTRY_CODES + join(localesWithoutISO3Country, COMMA_SEPARATOR));
     }
 
-    public static String convertISO31661Alpha2CodeToAlpha3Code(String alpha2Code){
+    public static String convertISO31661Alpha2CodeToAlpha3Code(String alpha2Code) {
         return isoAlpha2ToIsoAlpha3CodeMap.get(alpha2Code.toUpperCase(Locale.ROOT));
     }
 }
