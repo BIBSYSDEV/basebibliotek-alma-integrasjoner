d643685a8de6eca1246b81a2e7eaf0c9c199fa15
Tor Arne Torgersen <6398417+torarnet@users.noreply.github.com>
2024-12-19 09:46:42 +0100

    Ts 3 upgrade to java 21 (#873)
    * ts-3: upgraded java from 11 to 21
    
    upgraded pmd to add support for java 21
    
    * ts-3: fix ide warnings
    
    * ts-3: update to most recent pmd
    
    * ts-3: cleaned up unnecessary build steps in buildspec
    
    * ts-3: updated gradle wrapper

--- alma-integrations/src/main/java/no/sikt/lum/LibraryUserManagementHandler.java
+++ alma-integrations/src/main/java/no/sikt/lum/LibraryUserManagementHandler.java
@@ -63,6 +63,7 @@ public class LibraryUserManagementHandler implements RequestHandler<S3Event, Int
     private final transient Map<String, List<User>> usersPerAlmaInstanceMap = new ConcurrentHashMap<>();
 
     @JacocoGenerated
+    @SuppressWarnings("unused")
     public LibraryUserManagementHandler() {
         this(S3Driver.defaultS3Client().build(), new Environment(), SecretsManagerClient.builder()
                                                                         .region(Region.EU_WEST_1)
@@ -203,7 +204,7 @@ public class LibraryUserManagementHandler implements RequestHandler<S3Event, Int
         }
     }
 
-    private static class AlmaCodeAlmaApiKeyPair {
+    private static final class AlmaCodeAlmaApiKeyPair {
 
         @JsonProperty("almaCode")
         private transient String almaCode;
