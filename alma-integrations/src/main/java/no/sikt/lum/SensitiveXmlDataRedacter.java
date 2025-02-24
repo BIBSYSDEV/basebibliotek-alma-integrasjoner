c7d3a6f4915f6fd6146de03d73e689c1e2e81a7a
Tor Arne Torgersen <6398417+torarnet@users.noreply.github.com>
2025-02-24 15:58:06 +0100

    ts-31: create regex patterns at construct time instead of execution time to increase performance

--- alma-integrations/src/main/java/no/sikt/lum/SensitiveXmlDataRedacter.java
+++ alma-integrations/src/main/java/no/sikt/lum/SensitiveXmlDataRedacter.java
@@ -1,23 +1,42 @@
 package no.sikt.lum;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 import no.sikt.commons.Redacter;
 
 public class SensitiveXmlDataRedacter implements Redacter {
 
-    public static final String PATTERN_STRING = "<%s(.*?)</%s>";
-    public static final String REPLACEMENT_STRING = "<%s>redacted</%s>";
-    private final Set<String> tags = Set.of("aut", "password");
+    private static final String PATTERN_STRING = "<%s(.*?)</%s>";
+    private static final String REPLACEMENT_STRING = "<%s>redacted</%s>";
+    private static final Set<String> DEFAULT_TAGS = Set.of("aut", "password");
+
+    private final Map<String, Pattern> patterns;
+    private final Map<String, String> replacements;
 
     /**
      * Redacts content of tags in input based on a set of xml tags. Tag descriptions or properties will be removed.
      **/
+    public SensitiveXmlDataRedacter() {
+        this(DEFAULT_TAGS);
+    }
+
+    public SensitiveXmlDataRedacter(Set<String> tags) {
+        patterns = new HashMap<>();
+        replacements = new HashMap<>();
+
+        for (String tag : tags) {
+            patterns.put(tag, Pattern.compile(String.format(PATTERN_STRING, tag, tag)));
+            replacements.put(tag, String.format(REPLACEMENT_STRING, tag, tag));
+        }
+    }
+
     @Override
     public String redact(String input) {
         var output = input;
 
-        for (String tag : tags) {
+        for (String tag : patterns.keySet()) {
             output = replaceUsingPattern(output, tag);
         }
 
@@ -25,12 +44,9 @@ public class SensitiveXmlDataRedacter implements Redacter {
     }
 
     private String replaceUsingPattern(String input, String tag) {
-        var patternString = String.format(PATTERN_STRING, tag, tag);
-        var pattern = Pattern.compile(patternString);
-        var matcher = pattern.matcher(input);
-        var replacementString = String.format(REPLACEMENT_STRING, tag, tag);
-
-        return matcher.replaceAll(replacementString);
+        return patterns.get(tag)
+                   .matcher(input)
+                   .replaceAll(replacements.get(tag));
     }
 
 }
