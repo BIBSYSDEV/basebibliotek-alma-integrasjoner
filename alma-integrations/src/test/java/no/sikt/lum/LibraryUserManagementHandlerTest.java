8ffaf35f211888f1830b48253d1a4bd6e8eb1ae3
Tor Arne Torgersen <6398417+torarnet@users.noreply.github.com>
2025-12-02 15:52:25 +0100

    ts-147: jaxb failed when used in parallell processing. changed to parallell processing of users
    i dont know if users can be more than one, even if it is a list. im testing this now with some additional logging

--- alma-integrations/src/test/java/no/sikt/lum/LibraryUserManagementHandlerTest.java
+++ alma-integrations/src/test/java/no/sikt/lum/LibraryUserManagementHandlerTest.java
@@ -20,7 +20,6 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.StringContains.containsString;
-import static org.hamcrest.core.StringStartsWith.startsWith;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
